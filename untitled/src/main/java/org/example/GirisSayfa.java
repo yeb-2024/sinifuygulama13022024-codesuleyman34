1bb2aff054321c94078cbc772d43e4aa610ae5b6
suleymanenderkacar <kacarsuleymanender@gmail.com>
2025-02-13 18:08:47 +0300

    Giriş Sayfası oluşturuldu ve Kullanıcı Adı eklendi.

---
+++ untitled/src/main/java/org/example/GirisSayfa.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GirisSayfa
+{
+    JFrame frame;
+    JTextField kullaniciAdi;
+
+    public GirisSayfa()
+    {
+        kullaniciAdi = new JTextField();
+        kullaniciAdi.setPreferredSize(new Dimension(200,30));
+
+        frame = new JFrame("Giriş Sayfası");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(3,1));
+
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        panel.add(kullaniciAdi);
+
+        frame.add(new JLabel("Kullanıcı Adı:"));
+        frame.add(kullaniciAdi);
+
+        frame.add(panel);
+        frame.setSize(500,400);
+        frame.setVisible(true);
+
+
+    }
+}
